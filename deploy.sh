1cbd153b0cad22c4544ef10435f8d2a51aa9126d
pugtech <pugtech@dev.pugsnot.org>
2023-07-21 18:11:27 -0400

    new post

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m "new post"
 git push
 
-ansible-playbook deploy.yml --user dev --ask-become-pass -i ../hosts.ini
+ansible-playbook deploy.yml --user pugtech --ask-become-pass -i ../ansible/inventory/hosts
